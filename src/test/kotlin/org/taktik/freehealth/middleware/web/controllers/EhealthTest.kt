bb8889988e3e50d2496d4186f88494472201c40c
Geoffroy Piroux <geoffroy.piroux@b12-consulting.com>
2018-04-12 14:33:42 +0200

    Fix assurability test (problem with passPhrase, and some parameters)

--- src/test/kotlin/org/taktik/freehealth/middleware/web/controllers/EhealthTest.kt
+++ src/test/kotlin/org/taktik/freehealth/middleware/web/controllers/EhealthTest.kt
@@ -27,11 +27,14 @@ open class EhealthTest {
     @Value("\${org.taktik.icure.keystore2.ssin}") var ssin2 : String? = null
     @Value("\${org.taktik.icure.keystore1.password}") var password1 : String? = null
     @Value("\${org.taktik.icure.keystore2.password}") var password2 : String? = null
+    @Value("\${org.taktik.icure.keystore1.nihii}") var nihii1 : String? = null
+    @Value("\${org.taktik.icure.keystore2.nihii}") var nihii2 : String? = null
+    @Value("\${org.taktik.icure.keystore1.name}") var name1 : String? = null
+    @Value("\${org.taktik.icure.keystore2.name}") var name2 : String? = null
 
     protected fun uploadKeystore(path: String, port: Int, restTemplate: TestRestTemplate): UUID? {
         val map = LinkedMultiValueMap<String, Any>()
         map.add("file", FileSystemResource(path))
-        val hcpId = UUID.randomUUID()
         val result = restTemplate.postForObject("http://localhost:$port/sts/keystore", HttpEntity(map, HttpHeaders().apply { contentType = MediaType.MULTIPART_FORM_DATA }), UUIDType::class.java)
         val keystoreId = result.uuid
         return keystoreId
@@ -39,7 +42,7 @@ open class EhealthTest {
 
     protected fun register(restTemplate: TestRestTemplate, port: Int, ssin: String, passPhrase: String): Triple<UUID?, String, String> {
         val keystoreId = uploadKeystore((MyTestsConfiguration::class).java.getResource("${ssin}.acc-p12").path, port, restTemplate)
-        val res = restTemplate.getForObject("http://localhost:$port/sts/token/$keystoreId?passPhrase=$passPhrase&ssin=$ssin", SamlTokenResult::class.java)
+        val res = restTemplate.getForObject("http://localhost:$port/sts/token/$keystoreId?passPhrase={passPhrase}&ssin=$ssin", SamlTokenResult::class.java, passPhrase)
         val tokenId = res.tokenId
         return Triple(keystoreId, tokenId.toString(), passPhrase)
     }
