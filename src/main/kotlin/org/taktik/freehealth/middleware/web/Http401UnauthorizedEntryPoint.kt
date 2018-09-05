8bc49a0434252e103d20342d8d8f609406f7e518
Antoine Duchateau <ad@taktik.com>
2018-09-05 09:31:44 +0200

    DFixed missing Http

---
+++ src/main/kotlin/org/taktik/freehealth/middleware/web/Http401UnauthorizedEntryPoint.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2018 Taktik SA
+ *
+ * This file is part of iCureBackend.
+ *
+ * iCureBackend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * iCureBackend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with iCureBackend.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.taktik.freehealth.middleware.web
+
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import java.io.IOException
+
+class Http401UnauthorizedEntryPoint : AuthenticationEntryPoint {
+    @Throws(IOException::class, ServletException::class)
+    override fun commence(request: HttpServletRequest,
+                          response: HttpServletResponse,
+                          authException: AuthenticationException) {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+    }
+}
