6267552869c64dfc6d7de69374386825135d84d2
Antoine Duchateau <ad@taktik.com>
2018-10-11 13:06:24 +0200

    Bumped version and add sendMessage test

--- src/test/kotlin/org/taktik/freehealth/middleware/web/controllers/EhboxControllerTest.kt
+++ src/test/kotlin/org/taktik/freehealth/middleware/web/controllers/EhboxControllerTest.kt
@@ -1,6 +1,7 @@
 package org.taktik.freehealth.middleware.web.controllers
 
 
+import com.google.gson.Gson
 import org.assertj.core.api.Assertions
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -9,8 +10,11 @@ import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.context.annotation.Import
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
 import org.springframework.test.context.junit4.SpringRunner
 import org.taktik.freehealth.middleware.MyTestsConfiguration
+import org.taktik.freehealth.middleware.dto.ehbox.DocumentMessage
 
 
 @RunWith(SpringRunner::class)
@@ -28,7 +32,7 @@ class EhboxControllerTest : EhealthTest(){
     @Test
     fun getInfo(){
         val (keystoreId, tokenId, passPhrase) = register(restTemplate!!, port, ssin1!!, password1!!)
-        val ehboxInfo =  this.restTemplate.getForObject("http://localhost:$port/ehbox?keystoreId=$keystoreId&tokenId=$tokenId&passPhrase=$passPhrase", String::class.java)
+        val ehboxInfo =  this.restTemplate.exchange("http://localhost:$port/ehbox", HttpMethod.GET, HttpEntity<Void>(createHeaders(null, null, keystoreId, tokenId, passPhrase)), String::class.java)
         Assertions.assertThat(ehboxInfo != null)
     }
 
@@ -36,7 +40,7 @@ class EhboxControllerTest : EhealthTest(){
     fun loadMessage() {
         val boxType = "INBOX"
         val (keystoreId, tokenId, passPhrase) = register(restTemplate!!, port, ssin1!!, password1!!)
-        val messages = this.restTemplate.getForObject("http://localhost:$port/ehbox/$boxType?keystoreId=$keystoreId&tokenId=$tokenId&passPhrase=$passPhrase&limit=100", String::class.java)
+        val messages = this.restTemplate.exchange("http://localhost:$port/ehbox/$boxType?limit=100", HttpMethod.GET, HttpEntity<Void>(createHeaders(null, null, keystoreId, tokenId, passPhrase)), String::class.java)
         Assertions.assertThat(messages != null)
     }
 
@@ -45,7 +49,7 @@ class EhboxControllerTest : EhealthTest(){
         val idMessage = "1000010173227"
         val boxType = "INBOX"
         val (keystoreId, tokenId, passPhrase) = register(restTemplate!!, port, ssin1!!, password1!!)
-        val fullMessages = this.restTemplate.getForObject("http://localhost:$port/ehbox/$boxType/$idMessage?keystoreId=$keystoreId&tokenId=$tokenId&passPhrase=$passPhrase", String::class.java)
+        val fullMessages = this.restTemplate.exchange("http://localhost:$port/ehbox/$boxType/$idMessage", HttpMethod.GET, HttpEntity<Void>(createHeaders(null, null, keystoreId, tokenId, passPhrase)), String::class.java)
         Assertions.assertThat(fullMessages != null)
     }
 
@@ -56,7 +60,7 @@ class EhboxControllerTest : EhealthTest(){
         val messageIds = arrayListOf("1000010173227")
 
         val (keystoreId, tokenId, passPhrase) = register(restTemplate!!, port, ssin1!!, password1!!)
-        val message = this.restTemplate.postForObject("http://localhost:$port/ehbox/move/from/$source/to/$destination?keystoreId=$keystoreId&tokenId=$tokenId&passPhrase=$passPhrase", messageIds, String::class.java)
+        val message = this.restTemplate.exchange("http://localhost:$port/ehbox/move/from/$source/to/$destination", HttpMethod.GET, HttpEntity<List<String>>(messageIds, createHeaders(null, null, keystoreId, tokenId, passPhrase)), String::class.java)
         Assertions.assertThat(message != null)
     }
 
@@ -65,7 +69,70 @@ class EhboxControllerTest : EhealthTest(){
         val messageIds = arrayListOf("1000010173227")
         val source = "BININBOX"
         val (keystoreId, tokenId, passPhrase) = register(restTemplate!!, port, ssin1!!, password1!!)
-        val message = this.restTemplate.postForObject("http://localhost:$port/ehbox/move/from/$source?keystoreId=$keystoreId&tokenId=$tokenId&passPhrase=$passPhrase", messageIds, String::class.java)
+        val message = this.restTemplate.exchange("http://localhost:$port/ehbox/move/from/$source", HttpMethod.GET, HttpEntity<List<String>>(messageIds, createHeaders(null, null, keystoreId, tokenId, passPhrase)), String::class.java)
+        Assertions.assertThat(message != null)
+    }
+
+    @Test
+    fun sendMessage(){
+        val (keystoreId, tokenId, passPhrase) = register(restTemplate!!, port, ssin1!!, password1!!)
+        val msg = Gson().fromJson(
+            """{
+  "id": null,
+  "publicationId": null,
+  "publicationDateTime": 20181001,
+  "expirationDateTime": 20190101,
+  "customMetas": {},
+  "document": {
+    "title": "C’est un test",
+    "textContent": "Body text",
+    "filename": "rsp.txt",
+    "mimeType": "text/plain",
+    "signing": null
+  },
+  "freeText": null,
+  "freeInformationTableTitle": null,
+  "freeInformationTableRows": {},
+  "patientInss": null,
+  "annex": [],
+  "copyMailTo": [],
+  "documentTitle": null,
+  "annexList": [],
+  "useReceivedReceipt": false,
+  "useReadReceipt": false,
+  "hasAnnex": false,
+  "hasFreeInformations": false,
+  "important": true,
+  "encrypted": false,
+  "usePublicationReceipt": false,
+  "destinations": [
+    {
+      "identifierType": {
+        "type": "NIHII"
+      },
+      "id": "19234011",
+      "quality": "DOCTOR",
+      "applicationId": null,
+      "lastName": null,
+      "firstName": null,
+      "organizationName": null,
+      "personInOrganisation": null
+    }
+  ],
+  "sender": {
+    "identifierType": {
+      "type": "NIHII"
+    },
+    "id": "11478761",
+    "quality": "DOCTOR",
+    "applicationId": null,
+    "lastName": null,
+    "firstName": null,
+    "organizationName": null,
+    "personInOrganisation": null
+  }
+}""".trimIndent(), DocumentMessage::class.java)
+        val message = this.restTemplate.exchange("http://localhost:$port/ehbox?publicationReceipt=true&receptionReceipt=true&readReceipt=true", HttpMethod.POST, HttpEntity<DocumentMessage>(msg, createHeaders(null, null, keystoreId, tokenId, passPhrase)), String::class.java)
         Assertions.assertThat(message != null)
     }
 }
