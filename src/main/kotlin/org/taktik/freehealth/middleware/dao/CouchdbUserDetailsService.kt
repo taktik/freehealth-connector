32ed764280edbea8f46799c8bc8d4c99caf9cd66
Antoine Duchateau <ad@taktik.be>
2019-02-13 13:43:21 +0100

    Fixed auth with couchdb

--- src/main/kotlin/org/taktik/freehealth/middleware/dao/CouchdbUserDetailsService.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/dao/CouchdbUserDetailsService.kt
@@ -1,29 +1,28 @@
 package org.taktik.freehealth.middleware.dao
 
 import com.google.gson.Gson
+import org.apache.commons.codec.digest.DigestUtils
 import org.eclipse.jetty.client.HttpClient
 import org.eclipse.jetty.client.util.BasicAuthentication
+import org.eclipse.jetty.http.HttpHeader
 import org.springframework.cache.Cache
 import org.springframework.cache.CacheManager
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import java.net.URI
+import java.util.Base64
 import java.util.UUID
 
 class CouchdbUserDetailsService(val httpClient:HttpClient, val couchDbProperties: CouchDbProperties, val cacheManager : CacheManager, val gson: Gson = Gson()) : UserDetailsService {
-    override fun loadUserByUsername(username: String): UserDetails = cacheManager.getCache("USERS").get(username) {
+    override fun loadUserByUsername(username: String): UserDetails? = cacheManager.getCache("USERS")?.get(username) {
 
         val uri = URI.create(couchDbProperties.url + "/" + couchDbProperties.dbName + "/" + UUID.fromString(username).toString())
-        val authn = BasicAuthentication.BasicResult(uri, couchDbProperties.username, couchDbProperties.password)
         val request = httpClient.newRequest(uri)
+        request.header(HttpHeader.AUTHORIZATION, "Basic ${Base64.getEncoder().encodeToString("${couchDbProperties.username}:${couchDbProperties.password}".toByteArray())}");
 
-        authn.apply(request)
-
-        request.send().contentAsString
-
-        val user = gson.fromJson(request.send().contentAsString, User::class.java)
-        user.authorities.add(SimpleGrantedAuthority("ROLE_USER"))
-         user
+        gson.fromJson(request.send().contentAsString, User::class.java)?.apply {
+            authorities.add(SimpleGrantedAuthority("ROLE_USER"))
+        }
     }
 }
