c77ab6b0a667715578516997644b081af80bc370
LAPSAM5\Sam Jocqué <sam@figac.be>
2019-09-06 14:15:45 +0200

    TOP-702 R50Z47 conditional: before 01/07/2019 0000000, after 01/07/2019 +0000000

--- src/main/kotlin/org/taktik/freehealth/middleware/format/efact/BelgianInsuranceInvoicingFormatWriter.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/format/efact/BelgianInsuranceInvoicingFormatWriter.kt
@@ -397,7 +397,12 @@ class BelgianInsuranceInvoicingFormatWriter(private val writer: Writer) {
         ws.write("33", icd.personalInterventionCoveredByThirdPartyCode?. let { if (it >= 0) it else 0 } ?: 0)//MAF Zone 33 todo //Mettre 1 si a charge du medecin
         ws.write("34", (icd.sideCode?: InvoicingSideCode.None).code)
         ws.write("35", sender.conventionCode)
-        ws.write("47", "+0000000")
+        //47 conditional: before 01/07/2019 0000000, after 01/07/2019 +0000000
+        ws.write("47",
+            when {
+                invoicingMonth >= 7 && invoicingYear >= 2019 -> "+0000000"
+                else -> "00000000"
+            })
         //ws.write("49",icd.gnotionNihii)
         ws.write("49", when {
             icd.gnotionNihii?.isNotEmpty() == true -> icd.gnotionNihii
