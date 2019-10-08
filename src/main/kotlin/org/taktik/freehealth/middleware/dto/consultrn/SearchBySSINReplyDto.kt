96bdb5c937e5c30ab41402eae6c0bd632090ac69
Antoine Duchâteau <ad@taktik.com>
2019-10-08 12:36:33 +0200

    More DTOs for consultrn

--- src/main/kotlin/org/taktik/freehealth/middleware/dto/consultrn/SearchBySSINReplyDto.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/dto/consultrn/SearchBySSINReplyDto.kt
@@ -1,7 +1,9 @@
 package org.taktik.freehealth.middleware.dto.consultrn
 
+import be.fgov.ehealth.consultrn._1_0.core.ErrorType
 import be.fgov.ehealth.consultrn._1_0.core.PersonType
 import be.fgov.ehealth.consultrn._1_0.protocol.ConsultRnReplyType
+import org.taktik.freehealth.middleware.dto.common.StatusDto
 
 import javax.xml.bind.annotation.XmlAccessType
 import javax.xml.bind.annotation.XmlAccessorType
@@ -10,4 +12,4 @@ import javax.xml.bind.annotation.XmlRootElement
 import javax.xml.bind.annotation.XmlType
 import java.io.Serializable
 
-class SearchBySSINReplyDto(var person: ConsultRnPersonDto? = null) : ConsultRnReplyDto(), Serializable
+class SearchBySSINReplyDto(status: StatusDto? = null, id: String? = null, errorInformations: List<ErrorType>? = null, var person: ConsultRnPersonDto? = null) : ConsultRnReplyDto(status, id, errorInformations), Serializable
