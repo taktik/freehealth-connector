ffdb6ec395d72d50e89798a1913ce238c0375e44
Antoine Duchâteau <ad@taktik.com>
2019-10-24 13:55:36 +0200

    Horrible temporary hack that will need to be reverted

--- src/main/java/org/taktik/connector/technical/handler/ConnectionTimeOutHandler.java
+++ src/main/java/org/taktik/connector/technical/handler/ConnectionTimeOutHandler.java
@@ -20,11 +20,11 @@ public class ConnectionTimeOutHandler extends AbstractSOAPHandler {
       String endpoint = (String) context.get("javax.xml.ws.service.endpoint.address");
       String[] parts = endpoint != null ? endpoint.split("/+") : null;
       String endpointProperty = (parts != null && parts.length>=4) ? parts[parts.length-2] + "." + parts[parts.length-1] : null;
-      String requestTimeOut = this.getDuration("connector.soaphandler.connection.request.timeout", endpointProperty);
+      String requestTimeOut = "120000"; //this.getDuration("connector.soaphandler.connection.request.timeout", endpointProperty);
       LOG.debug("Setting request timeout on: {} milliseconds.", requestTimeOut);
       context.put("com.sun.xml.internal.ws.request.timeout", requestTimeOut);
       context.put("connector.soaphandler.connection.request.timeout", requestTimeOut);
-      String connectTimeOut = this.getDuration("connector.soaphandler.connection.connection.timeout", endpointProperty);
+      String connectTimeOut = "120000"; //this.getDuration("connector.soaphandler.connection.connection.timeout", endpointProperty);
       LOG.debug("Setting connect timeout on: {} milliseconds.", connectTimeOut);
       context.put("com.sun.xml.internal.ws.connect.timeout", connectTimeOut);
       context.put("connector.soaphandler.connection.connection.timeout", connectTimeOut);
@@ -34,10 +34,10 @@ public class ConnectionTimeOutHandler extends AbstractSOAPHandler {
    private String getDuration(String timeoutProp, String endpointProperty) {
       String endpointTimeoutProp = endpointProperty != null ? timeoutProp + "." + endpointProperty : null;
       if (endpointTimeoutProp != null && (this.config.hasProperty(endpointTimeoutProp) || this.config.hasDurationProperty(endpointTimeoutProp))) {
-         return this.config.hasDurationProperty(endpointTimeoutProp) ? Long.toString(this.config.getDurationProperty("connector.soaphandler.connection.request.timeout", 60L, TimeUnit.SECONDS).convert(TimeUnit.MILLISECONDS)) : this.config.getProperty(endpointTimeoutProp, "30000");
+         return this.config.hasDurationProperty(endpointTimeoutProp) ? Long.toString(this.config.getDurationProperty("connector.soaphandler.connection.request.timeout", 60L, TimeUnit.SECONDS).convert(TimeUnit.MILLISECONDS)) : this.config.getProperty(endpointTimeoutProp, "60000");
       }
 
-      return this.config.hasDurationProperty(timeoutProp) ? Long.toString(this.config.getDurationProperty("connector.soaphandler.connection.request.timeout", 60L, TimeUnit.SECONDS).convert(TimeUnit.MILLISECONDS)) : this.config.getProperty(timeoutProp, "30000");
+      return this.config.hasDurationProperty(timeoutProp) ? Long.toString(this.config.getDurationProperty("connector.soaphandler.connection.request.timeout", 60L, TimeUnit.SECONDS).convert(TimeUnit.MILLISECONDS)) : this.config.getProperty(timeoutProp, "60000");
    }
 
    public boolean handleFault(SOAPMessageContext context) {
