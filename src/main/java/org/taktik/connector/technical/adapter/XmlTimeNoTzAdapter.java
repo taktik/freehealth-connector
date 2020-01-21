9b2288b6b724125d69fb45c59e6ff18ee1a0c4d7
Antoine Duchâteau <ad@taktik.com>
2020-01-21 11:08:31 +0100

    Fixed 3.178 incompatibility

--- src/main/java/org/taktik/connector/technical/adapter/XmlTimeNoTzAdapter.java
+++ src/main/java/org/taktik/connector/technical/adapter/XmlTimeNoTzAdapter.java
@@ -5,12 +5,8 @@ import org.joda.time.DateTimeZone;
 import org.taktik.connector.technical.utils.DateUtils;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-public class XmlTimeNoTzAdapter extends XmlAdapter<String, DateTime> {
+public class XmlTimeNoTzAdapter extends XmlTimeAdapter {
    public static DateTimeZone noTimeZone = DateTimeZone.forOffsetMillis(3600001);
-
-   public DateTime unmarshal(String value) throws Exception {
-      return DateUtils.parseTime(value);
-   }
 
    public String marshal(DateTime value) throws Exception {
       return value.getZone() == noTimeZone ? DateUtils.printTimeWithoutTimeZone(value) : super.marshal(value);
