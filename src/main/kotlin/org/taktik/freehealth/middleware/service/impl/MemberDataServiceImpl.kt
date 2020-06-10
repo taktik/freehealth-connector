500834b86e3edfae2736dff0921130a4b3abb729
Maxime Mennechet <maxime.mennechet@gmail.com>
2020-06-10 16:04:48 +0200

    Fixed mda async

--- src/main/kotlin/org/taktik/freehealth/middleware/service/impl/MemberDataServiceImpl.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/service/impl/MemberDataServiceImpl.kt
@@ -99,6 +99,7 @@ import org.taktik.connector.technical.service.etee.CryptoFactory
 import org.taktik.connector.technical.service.keydepot.KeyDepotService
 import org.taktik.connector.technical.service.keydepot.impl.KeyDepotManagerImpl
 import org.taktik.connector.technical.service.sts.security.impl.KeyStoreCredential
+import org.taktik.connector.technical.utils.ConnectorIOUtils
 import org.taktik.connector.technical.utils.ConnectorXmlUtils
 import org.taktik.connector.technical.utils.IdentifierType
 import org.taktik.connector.technical.utils.MarshallerHelper
@@ -115,6 +116,7 @@ import org.taktik.freehealth.middleware.service.STSService
 import org.taktik.icure.cin.saml.extensions.AttributeQueryList
 import org.taktik.icure.cin.saml.extensions.ExtensionsType
 import org.taktik.icure.cin.saml.extensions.Facet
+import org.taktik.icure.cin.saml.extensions.ResponseList
 import org.taktik.icure.cin.saml.oasis.names.tc.saml._2_0.protocol.Response
 import org.w3c.dom.Element
 import org.w3c.dom.Node
@@ -318,13 +320,17 @@ class MemberDataServiceImpl(val stsService: STSService, keyDepotService: KeyDepo
         val b64 = Base64.getEncoder()
 
         response.`return`.msgResponses?.map{it ->
-            val blob = DomainBlobMapper.mapToBlob(it.getDetail())
+            val blob = DomainBlobMapper.mapToBlob(it.detail)
             var data: ByteArray? = blob.content
             val unsealedData = crypto.unseal(Crypto.SigningPolicySelector.WITHOUT_NON_REPUDIATION, data).contentAsByte
             val encryptedKnownContent = MarshallerHelper(EncryptedKnownContent::class.java, EncryptedKnownContent::class.java).toObject(unsealedData)
-
-
-
+            val responseList = MarshallerHelper(ResponseList::class.java, ResponseList::class.java).toObject(
+                if(encryptedKnownContent.businessContent.contentEncoding == "deflate")
+                    ConnectorIOUtils.decompress(encryptedKnownContent.businessContent.value) else encryptedKnownContent.businessContent.value
+            )
+            responseList.responses.map {
+                it
+            }
         }
 
 
