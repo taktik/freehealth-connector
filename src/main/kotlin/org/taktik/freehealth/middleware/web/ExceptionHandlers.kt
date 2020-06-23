726c95a15f8546a532d633924c8085baad92e806
Ludovic Guillaume <ludovic.guillaume@beoogo.be>
2020-06-23 19:18:32 +0200

    Fix error handling for metrics

--- src/main/kotlin/org/taktik/freehealth/middleware/web/ExceptionHandlers.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/web/ExceptionHandlers.kt
@@ -1,46 +1,51 @@
 package org.taktik.freehealth.middleware.web
 
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.taktik.connector.technical.exception.TechnicalConnectorException
+import org.taktik.freehealth.middleware.dto.ExceptionDto
 import org.taktik.freehealth.middleware.exception.MissingKeystoreException
 import org.taktik.freehealth.middleware.exception.MissingTokenException
-import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletRequest
 import javax.xml.ws.soap.SOAPFaultException
 
 /**
  * Defines custom exception handlers.
- *
- * Uses [HttpServletResponse.sendError] to redirect to the servlet error page which will be handled by [BasicErrorController].
- *
- * **NOTE**: Adding [ResponseStatus] to the methods breaks the expected behavior.
  */
 @ControllerAdvice
 class ExceptionHandlers {
-    companion object {
-        private const val DEFAULT_EXCEPTION_MESSAGE = "unknown reason"
-    }
-
     @ExceptionHandler(TechnicalConnectorException::class)
-    fun handleTechnicalConnectorException(response: HttpServletResponse, exception: TechnicalConnectorException) {
-        response.sendError(exception.category.httpStatus.value(), exception.message ?: DEFAULT_EXCEPTION_MESSAGE)
-    }
+    fun handleTechnicalConnectorException(request: HttpServletRequest, exception: TechnicalConnectorException) =
+            ExceptionDto(exception.category.httpStatus, exception, request.servletPath).toResponseEntity()
 
     @ExceptionHandler(MissingKeystoreException::class, MissingTokenException::class)
-    fun handleUnauthorizedException(response: HttpServletResponse, exception: Exception) {
-        response.sendError(HttpStatus.UNAUTHORIZED.value(), exception.message ?: DEFAULT_EXCEPTION_MESSAGE)
-    }
+    fun handleUnauthorizedException(request: HttpServletRequest, exception: Exception) =
+            ExceptionDto(HttpStatus.UNAUTHORIZED, exception, request.servletPath).toResponseEntity()
 
     @ExceptionHandler(IllegalArgumentException::class)
-    fun handleIllegalArgumentException(response: HttpServletResponse, exception: IllegalArgumentException) {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), exception.message ?: DEFAULT_EXCEPTION_MESSAGE)
-    }
+    fun handleIllegalArgumentException(request: HttpServletRequest, exception: IllegalArgumentException) =
+            ExceptionDto(HttpStatus.BAD_REQUEST, exception, request.servletPath).toResponseEntity()
 
     @ExceptionHandler(SOAPFaultException::class)
-    fun handleSoapFaultException(response: HttpServletResponse, exception: SOAPFaultException) {
-        response.sendError(HttpStatus.BAD_GATEWAY.value(), exception.message ?: DEFAULT_EXCEPTION_MESSAGE)
+    fun handleSoapFaultException(request: HttpServletRequest, exception: SOAPFaultException) =
+            ExceptionDto(HttpStatus.BAD_GATEWAY, exception, request.servletPath).toResponseEntity()
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(request: HttpServletRequest, exception: Exception): ResponseEntity<ExceptionDto> {
+        val cause = exception.cause
+
+        if (cause != null) {
+            when (cause) {
+                is TechnicalConnectorException -> handleTechnicalConnectorException(request, cause)
+                is MissingKeystoreException -> handleUnauthorizedException(request, cause)
+                is MissingTokenException -> handleUnauthorizedException(request, cause)
+                is IllegalArgumentException -> handleIllegalArgumentException(request, cause)
+                is SOAPFaultException -> handleSoapFaultException(request, cause)
+            }
+        }
+
+        return ExceptionDto(HttpStatus.INTERNAL_SERVER_ERROR, exception, request.servletPath).toResponseEntity()
     }
 }
