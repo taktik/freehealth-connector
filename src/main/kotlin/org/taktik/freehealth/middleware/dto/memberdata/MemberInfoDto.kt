32fda1b5e7f1e3b281f7674fb6003b5a89fd47c9
Maxime Mennechet <maxime.mennechet@gmail.com>
2020-06-24 22:38:49 +0200

    Fixed inputReference for MDA

--- src/main/kotlin/org/taktik/freehealth/middleware/dto/memberdata/MemberInfoDto.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/dto/memberdata/MemberInfoDto.kt
@@ -1,3 +1,3 @@
 package org.taktik.freehealth.middleware.dto.memberdata
 
-class MemberInfoDto(var ssin:String? = null, var ioMembership:String? = null, var hospitalized: Boolean = false)
+class MemberInfoDto(var ssin:String? = null, var ioMembership:String? = null, var hospitalized: Boolean = false, var uniqId: String)
--- src/main/kotlin/org/taktik/freehealth/middleware/service/impl/MemberDataServiceImpl.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/service/impl/MemberDataServiceImpl.kt
@@ -199,7 +199,7 @@ class MemberDataServiceImpl(val stsService: STSService, keyDepotService: KeyDepo
         val samlFacets = mdaRequest.facets?.map { mapper.map(it, Facet::class.java) }
 
         val attrQueries = mdaRequest.members.map {
-            val inputRef = "" + IdGeneratorFactory.getIdGenerator().generateId()
+            val inputRef = it.uniqId
             val requestId = IdGeneratorFactory.getIdGenerator("xsid").generateId()
             getAttrQuery(inputRef, issueInstant, samlFacets, it.hospitalized, requestType, hcpNihii, it.ssin, io, it.ioMembership, startDate, endDate)
         }
