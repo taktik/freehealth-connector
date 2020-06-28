bce449fa326559b30298918457198b5e9cc554d1
LAPSAM5\Sam Jocqué <sam@figac.be>
2020-06-28 13:38:45 +0200

    Correction for R50Z15 --> medicalhouse and nomenclature 109594  PTD --> put nihii R50Z47 --> +0000000 from 7/2019 (correction of invalid logic)

--- src/main/kotlin/org/taktik/freehealth/middleware/format/efact/BelgianInsuranceInvoicingFormatWriter.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/format/efact/BelgianInsuranceInvoicingFormatWriter.kt
@@ -376,6 +376,7 @@ class BelgianInsuranceInvoicingFormatWriter(private val writer: Writer) {
         ws.write("13",990)
         if (sender.isMedicalHouse) ws.write("14", sender.nihii)
         if (!sender.isMedicalHouse) ws.write("15", icd.doctorIdentificationNumber)
+        if (sender.isMedicalHouse && icd.codeNomenclature == 109594L) ws.write("15", sender.nihii)
         //ws.write("16", if (sender.isMedicalHouse) 0 else if (icd.gnotionNihii == null || icd.gnotionNihii?.let { it.isEmpty() } == true) 1 else 4)
         ws.write("16",
                  when {
@@ -401,6 +402,7 @@ class BelgianInsuranceInvoicingFormatWriter(private val writer: Writer) {
         ws.write("47",
             when {
                 invoicingMonth >= 7 && invoicingYear >= 2019 -> "+0000000"
+                invoicingYear >= 2020 -> "+0000000"
                 else -> "00000000"
             })
         //ws.write("49",icd.gnotionNihii)
