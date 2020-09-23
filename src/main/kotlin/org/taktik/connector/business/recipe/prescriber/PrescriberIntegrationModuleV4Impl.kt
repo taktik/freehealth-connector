9b6d520c106e9d06ec8c2cf9af36035869b463a6
Maxime Mennechet <maxime.mennechet@gmail.com>
2020-09-23 16:49:03 +0200

    fixed

--- src/main/kotlin/org/taktik/connector/business/recipe/prescriber/PrescriberIntegrationModuleV4Impl.kt
+++ src/main/kotlin/org/taktik/connector/business/recipe/prescriber/PrescriberIntegrationModuleV4Impl.kt
@@ -121,8 +121,8 @@ class PrescriberIntegrationModuleV4Impl(stsService: STSService, keyDepotService:
             val request = CreatePrescriptionRequest()
             request.securedCreatePrescriptionRequest = createSecuredContentType(sealRequest(getCrypto(credential), etkRecipes[0] as EncryptionToken, helper.toXMLByteArray(params)))
 
-            //request.programId = vendorName ?: "freehealth-connector"
-            request.programId = propertyHandler.getProperty("programIdentification") ?: "freehealth-connector"
+            request.programId = vendorName ?: "freehealth-connector"
+            //request.programId = propertyHandler.getProperty("programIdentification") ?: "freehealth-connector"
             request.id = "id" + UUID.randomUUID().toString()
             request.issueInstant = DateTime.now()
 
