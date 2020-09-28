a4c374a7e865d8cd59cf86228d1e562b2a9150c9
Antoine Duchâteau <ad@taktik.com>
2020-09-28 14:38:31 +0200

    Fixed date format for end of year dates in recip-e and dmg

--- src/main/kotlin/org/taktik/freehealth/middleware/service/impl/RecipeV4ServiceImpl.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/service/impl/RecipeV4ServiceImpl.kt
@@ -162,7 +162,10 @@ class RecipeV4ServiceImpl(private val codeDao: CodeDao, private val stsService: 
             throw e
         }
 
-        val prescriptionId = service.createPrescription(keystore, samlToken, passPhrase, credential, hcpNihii, feedback, patient.ssin!!, prescription, selectedType, vision, vendorName, packageName, packageVersion, expirationDate ?: LocalDateTime.now().plusMonths(3).minusDays(1))
+        val unconstrainedDate = expirationDate ?: deliveryDate?.plusMonths(3)?.minusDays(1) ?: LocalDateTime.now().plusMonths(3).minusDays(1)
+        val limitDate =  LocalDateTime.now().plusYears(1).minusDays(1)
+
+        val prescriptionId = service.createPrescription(keystore, samlToken, passPhrase, credential, hcpNihii, feedback, patient.ssin!!, prescription, selectedType, vision, vendorName, packageName, packageVersion, if (unconstrainedDate.isAfter(limitDate)) limitDate else unconstrainedDate)
 
         val result = Prescription(Date(), "", prescriptionId!!)
 
