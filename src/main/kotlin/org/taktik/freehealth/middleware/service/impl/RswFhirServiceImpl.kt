bdbf8818931673c2b28acddc771e2d279145add9
Antoine Duchâteau <ad@taktik.com>
2020-11-23 16:38:52 +0100

    Cleanup

--- src/main/kotlin/org/taktik/freehealth/middleware/service/impl/RswFhirServiceImpl.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/service/impl/RswFhirServiceImpl.kt
@@ -13,7 +13,6 @@ import org.bouncycastle.cms.CMSSignerDigestMismatchException
 import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder
 import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient
 import org.jose4j.jwt.consumer.JwtConsumerBuilder
-import org.jose4j.jwt.consumer.JwtContext
 import org.springframework.stereotype.Service
 import org.taktik.freehealth.middleware.domain.rsw.Jwks
 import org.taktik.freehealth.middleware.domain.rsw.Jwt
@@ -26,6 +25,7 @@ import java.io.ByteArrayInputStream
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLEncoder
 import java.security.PublicKey
 import java.security.cert.CertificateFactory
 import java.security.cert.X509Certificate
@@ -35,19 +35,15 @@ import javax.mail.Multipart
 import javax.mail.Session
 import javax.mail.internet.MimeMessage
 
-
 fun HttpURLConnection.postForm(form: List<Pair<String, String>>): InputStream? {
     setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
     requestMethod = "POST"
     doOutput = true
     outputStream.bufferedWriter().use {
-        it.write(form.map { (k, v) -> "${k}=${java.net.URLEncoder.encode(v, Charsets.UTF_8.name())}" }
-                     .joinToString("&")
-                )
+        it.write(form.joinToString("&") { (k, v) -> "${k}=${URLEncoder.encode(v, Charsets.UTF_8.name())}" })
     }
     return if (responseCode == 200) inputStream else null
 }
-
 
 @Service
 class RswFhirServiceImpl(val stsService: STSService) : RswFhirService {
@@ -57,15 +53,6 @@ class RswFhirServiceImpl(val stsService: STSService) : RswFhirService {
     val audience = "api.rsw.clientid.fhir.careset"
     val objectMapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule())
     var rswPublicKey: PublicKey? = null
-
-    private fun getJwtTokenContext(rswPublicKey: PublicKey, jwtToken: String): JwtContext? {
-        val firstPassConsumer = JwtConsumerBuilder()
-            .setRequireJwtId().setRequireNotBefore()
-            .setRequireExpirationTime().setExpectedAudience(true, audience)
-            .setVerificationKey(rswPublicKey).setExpectedIssuer(issuer)
-            .setAllowedClockSkewInSeconds(120).build()
-        return firstPassConsumer.process(jwtToken) //Will throw an exception if invalid
-    }
 
     override fun search(nihii: String,
         clientId: String,
@@ -79,17 +66,18 @@ class RswFhirServiceImpl(val stsService: STSService) : RswFhirService {
         val hokPrivateKeys = KeyManager.getDecryptionKeys(keystore, passPhrase.toCharArray())
         val privateKey = hokPrivateKeys.entries.mapNotNull { it.value }.firstOrNull()
 
-        val accessToken = obtainAccessToken(clientId, clientSecret, nihii)
+        val accessToken = obtainAccessToken(clientId, clientSecret, nihii) ?: throw IllegalArgumentException("Cannot get access token")
         val bundle = loadFHIRSearchResult(clientType, nihii, accessToken, patientSsin)
 
         val multipart = Base64.getDecoder().decode(bundle.entry.mapNotNull { it.resource as? Binary }.first().data!!)
 
-        val session = Session.getDefaultInstance(System.getProperties(), null);
+        val session = Session.getDefaultInstance(System.getProperties(), null)
         return (MimeMessage(session, ByteArrayInputStream(multipart)).content as? Multipart)?.let { msg ->
             val bytes = msg.getBodyPart(0).inputStream.readBytes()
             val signedContent: CMSProcessable = CMSProcessableByteArray(bytes)
             val signedData = CMSSignedData(signedContent, msg.getBodyPart(1).inputStream)
 
+            @Suppress("UNUSED_VARIABLE")
             val signed = signedData.signerInfos.signers.any { signer ->
                 signedData.certificates.getMatches(signer.sid).filterIsInstance(X509CertificateHolder::class.java).any { ch ->
                     val cert: X509Certificate = JcaX509CertificateConverter().setProvider("BC").getCertificate(ch)
@@ -105,14 +93,19 @@ class RswFhirServiceImpl(val stsService: STSService) : RswFhirService {
             val recipient = JceKeyTransEnvelopedRecipient(privateKey!!)
 
             recInfo?.getContentStream(recipient)?.contentStream?.readBytes() //<-- Error here: bad padding: data hash wrong
-        } ?: throw IllegalStateException("Cannot decode")
+        } ?: throw IllegalStateException("Cannot decode data")
     }
+
+    private fun getJwtTokenContext(rswPublicKey: PublicKey, jwtToken: String) = JwtConsumerBuilder()
+        .setRequireJwtId().setRequireNotBefore()
+        .setRequireExpirationTime().setExpectedAudience(true, audience)
+        .setVerificationKey(rswPublicKey).setExpectedIssuer(issuer)
+        .setAllowedClockSkewInSeconds(120).build().process(jwtToken) //Will throw an exception if invalid
 
     private fun loadFHIRSearchResult(clientType: String,
         nihii: String,
-        accessToken: Unit,
-        patientSsin: String): Bundle {
-        return objectMapper.readValue(
+        accessToken: String,
+        patientSsin: String) = objectMapper.readValue(
             (URL("$baseSearchUrl|${patientSsin}").openConnection() as? HttpURLConnection)?.apply {
                 requestMethod = "GET"
                 setRequestProperty("Accept", "application/json")
@@ -127,9 +120,8 @@ class RswFhirServiceImpl(val stsService: STSService) : RswFhirService {
                     it.inputStream.readBytes()
                 }
             } ?: throw RuntimeException("Cannot create Fhir Api URL"), Bundle::class.java)
-    }
 
-    private fun obtainAccessToken(clientId: String, clientSecret: String, nihii: String) {
+    private fun obtainAccessToken(clientId: String, clientSecret: String, nihii: String) =
         (URL("https://jacc.reseausantewallon.be/is4acc/careset/v1/token").openConnection() as? HttpURLConnection)?.postForm( //Request token
             listOf(
                 "grant_type" to "client_credentials",
@@ -139,11 +131,8 @@ class RswFhirServiceImpl(val stsService: STSService) : RswFhirService {
                 "client_nihii" to nihii
                   ))?.let {
             val readText = it.bufferedReader().readText()
-            objectMapper.readValue(readText, Jwt::class.java)?.accessToken?.also { at ->
-                getJwtTokenContext(fetchRSWPublicKey(), at)
-            }
+            objectMapper.readValue(readText, Jwt::class.java)?.accessToken?.also { at -> getJwtTokenContext(fetchRSWPublicKey(), at) }
         }
-    }
 
     private fun fetchRSWPublicKey(): PublicKey =
         rswPublicKey ?: objectMapper.readValue(URL("https://jacc.reseausantewallon.be/is4acc/.well-known/openid-configuration").readBytes(), //load endpoints
