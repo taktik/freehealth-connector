ad6484f4d63d8ed2f77c0dbf9dfc0417ffd90b42
Antoine Duchâteau <ad@taktik.com>
2020-12-09 14:13:18 +0100

    Cleanup

--- src/main/java/org/taktik/connector/technical/config/ConfigFactory.java
+++ src/main/java/org/taktik/connector/technical/config/ConfigFactory.java
@@ -35,7 +35,7 @@ public final class ConfigFactory {
    }
 
    public static ConfigValidator getConfigValidator() {
-      return getConfigValidator((List)null);
+      return getConfigValidator(null);
    }
 
    public static ConfigValidator getConfigValidator(List<String> expectedProps) {
