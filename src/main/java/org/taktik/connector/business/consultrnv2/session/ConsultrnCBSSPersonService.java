8293c2f40aace3f4e20dfeefdc2f2fa84fb9c24d
Maxime Mennechet <maxime.mennechet@gmail.com>
2021-01-15 13:49:56 +0100

    Wip on rn consult new version

--- src/main/java/org/taktik/connector/business/consultrnv2/session/ConsultrnCBSSPersonService.java
+++
@@ -1,9 +0,0 @@
-package org.taktik.connector.business.consultrnv2.session;
-
-import be.fgov.ehealth.rn.cbsspersonservice.protocol.v1.RegisterPersonRequest;
-import be.fgov.ehealth.rn.cbsspersonservice.protocol.v1.RegisterPersonResponse;
-import org.taktik.connector.technical.exception.TechnicalConnectorException;
-
-public interface ConsultrnCBSSPersonService {
-   RegisterPersonResponse registerPerson(RegisterPersonRequest var1) throws TechnicalConnectorException;
-}
