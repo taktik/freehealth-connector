2d66e41e022bbb72f5f2d425168c02c2ab091a21
Gauvain Klug <gauvain@prismatech>
2022-03-24 09:39:27 +0100

    Fix SAMLToken quality
    SAMLToken quality was never set from call's parameters, always forced to "doctor"

--- src/main/java/org/taktik/connector/technical/service/sts/security/impl/SAMLSenderVouchesCredential.java
+++ src/main/java/org/taktik/connector/technical/service/sts/security/impl/SAMLSenderVouchesCredential.java
@@ -1,10 +1,11 @@
 package org.taktik.connector.technical.service.sts.security.impl;
 
+import org.taktik.connector.technical.exception.TechnicalConnectorException;
 import org.taktik.connector.technical.service.sts.security.Credential;
 import org.w3c.dom.Element;
 
 public class SAMLSenderVouchesCredential extends AbstractSAMLToken {
-   public SAMLSenderVouchesCredential(Element assertion, Credential credential) {
+   public SAMLSenderVouchesCredential(Element assertion, Credential credential) throws TechnicalConnectorException {
       super(assertion, credential);
    }
 }
