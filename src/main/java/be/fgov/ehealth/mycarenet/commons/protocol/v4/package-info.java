c33315e3fca5f1a69057da2d78b68ac7cb4aeb61
Maxime Mennechet <maxime.mennechet@gmail.com>
2022-11-16 12:35:53 +0100

    Added protocol v4 for MCN

---
+++ src/main/java/be/fgov/ehealth/mycarenet/commons/protocol/v4/package-info.java
@@ -0,0 +1,6 @@
+@javax.xml.bind.annotation.XmlSchema(
+  namespace = "urn:be:fgov:ehealth:mycarenet:commons:protocol:v4",
+  elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED
+)
+
+package be.fgov.ehealth.mycarenet.commons.protocol.v4;
