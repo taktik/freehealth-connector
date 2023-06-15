659f97b1805d8cf71affc6f5faf6139c02b1fb1d
adrian <>
2023-06-15 13:25:12 +0300

    eAttestV3 cancellation fixes

--- src/main/kotlin/org/taktik/freehealth/middleware/service/EattestV3Service.kt
+++ src/main/kotlin/org/taktik/freehealth/middleware/service/EattestV3Service.kt
@@ -69,7 +69,7 @@ interface EattestV3Service {
         patientFirstName: String,
         patientLastName: String,
         patientGender: String,
-        referenceDate: Int?,
+        referenceDate: Long?,
         eAttestRef: String,
         reason: String,
         attemptNbr: Int?
